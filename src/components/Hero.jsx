c358b42446a7877d952f6af69afba07510808e7a
Curtis Cadwallader <ccad99@comcast.net>
2025-02-22 11:08:04 -0800

    Finished responsiveness on Hero & Carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,30 @@
 import styles from "./Hero.module.css";
 import heroImage from "../assets/images/littleLemon/bruschetta-platter.jpg";
 import { Link } from "react-router-dom";
-import ReservationPage from "../pages/ReservationPage";
 
 function Hero() {
    return (
-      <section className={styles.hero}>
-         <div className={styles.heroText}>
-            <div className={styles.hero_}>
-               <h1>Little Lemon</h1>
-               <h4>Chicago</h4>
+      <section className={styles.container}>
+         <div className={styles.hero}>
+            <div className={styles.heroText}>
+               <div className={styles.heroTextHead}>
+                  <h1>Little Lemon</h1>
+                  <h2>Chicago</h2>
+               </div>
+               <p>
+                  We are a family owned Italian restaurant, focused on
+                  traditional recipes served with a modern twist.
+               </p>
+               <Link to="/reservations">
+                  <button className={styles.heroButton}>Reserve a Table</button>
+               </Link>
             </div>
-            <p className={styles.heroText}>
-               We are a family owned Italian restaurant, focused on traditional
-               recipes served with a modern twist.
-            </p>
-            <Link to="/reservations">
-               <button className={styles.heroButton}>Reserve A Table</button>
-            </Link>
+            <img
+               src={heroImage}
+               alt="Waiter holding platter of Bruschetta"
+               className={styles.heroImage}
+            />
          </div>
-         <img
-            src={heroImage}
-            alt="Waiter holding platter of Bruschetta"
-            className={styles.heroImage}
-         />
       </section>
    );
 }
